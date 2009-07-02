7ad0fd8b6542678b9de7a233eec560bd97f29adf
crazedsanity <crazedsanity@4b4c563d-1935-4de8-960c-fa1d13a43f2e>
2009-07-02 18:08:08 +0000

    Separate the class from the wrapper script.

---
+++ trunk/0.1/cli_logger.class.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * Created on Jul 1, 2009
+ * 
+ * SVN INFORMATION:::
+ * ------------------
+ * SVN Signature::::::: $Id$
+ * Last Author::::::::: $Author$ 
+ * Current Revision:::: $Revision$
+ * Repository Location: $HeadURL$ 
+ * Last Updated:::::::: $Date$
+ */
+
+//TODO: The script's version will become important if this is installed on different hosts, as version numbers might be different...
+//TODO: consider using the cs-webdblogger library for handling ALL logging.
+
+
+
+
+class cliWrapper extends cs_versionAbstract {
+	
+	/** Database object */
+	private $dbObj;
+	
+	/** The full command that was performed... */
+	private $fullCommand;
+	
+	//-------------------------------------------------------------------------
+	/**
+	 * Handle everything here: if there's something missing, an exception will 
+	 * be thrown and things will stop running.
+	 */
+	public function __construct($configFile) {
+		//set the version file location, a VERY important part of this system.
+		$this->set_version_file_location($configFile);
+		
+		if(file_exists($configFile)) {
+			$xmlParser = new cs_phpxmlParser(file_get_contents($configFile));
+		}
+		else {
+			throw new exception("missing configuration file");
+		}
+		
+		$this->parse_parameters();
+		$this->run_script();
+	}//end __construct()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	/**
+	 * Run the script here...
+	 */
+	public function run_script() {
+		$scriptId = $this->get_script_id();
+		$hostId = $this->get_host_id();
+		
+		//TODO: call the script here (fork?)
+		
+		//TODO: log the script's output into the database.
+	}//end run_script()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	/**
+	 * Rip out parameters meant for this wrapper script (vs. the script that it 
+	 * is wrapping).
+	 */
+	private function parse_parameters() {
+	}//end parse_parameters()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	/**
+	 * Connect the internal database object.
+	 */
+	private function connect_db() {
+		
+	}//end connect_db()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	/**
+	 * Determine what the ID of the script is (for database logging).
+	 */
+	private function get_script_id() {
+	}//end get_script_id()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	/**
+	 * Get the ID of the host it's running on (for database logging).
+	 */
+	private function get_host_id() {
+	}//end get_host_id()
+	//-------------------------------------------------------------------------
+	
+	
+}
+
+
+
+// Instantiating the class is all that is needed to get the script to run.
+$obj = new cliWrapper($configFile);
+
+
+?>
