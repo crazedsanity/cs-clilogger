1830800b3f5388ddcd4511d17627d5d476ba0312
crazedsanity <crazedsanity@4b4c563d-1935-4de8-960c-fa1d13a43f2e>
2009-07-02 18:35:56 +0000

    This is a dump of the database... in case I missed something (includes necessary version information in the comments as well).

--- trunk/0.1/docs/schema.mysql.sql
+++ trunk/0.1/docs/schema.mysql.sql
@@ -8,72 +8,94 @@
 -- Last Updated:::::::: $Date$
 --
 
---
--- HOST Table
---
-CREATE TABLE `cli_logger`.`cli_host_table` (
-  `host_id` int  NOT NULL AUTO_INCREMENT,
-  `host_name` text  NOT NULL,
-  PRIMARY KEY (`host_id`)
-)
-ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 2.10.3
+-- http://www.phpmyadmin.net
+-- 
+-- Generation Time: Jul 02, 2009 at 01:43 PM
+-- Server version: 5.0.22
+-- PHP Version: 5.1.6
 
+SET FOREIGN_KEY_CHECKS=0;
 
+SET AUTOCOMMIT=0;
+START TRANSACTION;
 
---
--- Internal logging table
---
-CREATE TABLE `cli_logger`.`cli_internal_log_table` (
-  `internal_log_id` int  NOT NULL AUTO_INCREMENT,
-  `log_timestamp` TIMESTAMP  NOT NULL DEFAULT NOW(),
-  `log_data` text  NOT NULL,
-  PRIMARY KEY (`internal_log_id`)
-)
-ENGINE = InnoDB;
+-- 
+-- Database: `cli_logger`
+-- 
 
+-- --------------------------------------------------------
 
+-- 
+-- Table structure for table `cli_host_table`
+-- 
 
+CREATE TABLE `cli_host_table` (
+  `host_id` int(11) NOT NULL auto_increment,
+  `host_name` text NOT NULL,
+  PRIMARY KEY  (`host_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Scripts table
--- NOTE: it may be important, when calculating max/average run times, to link
---	the script to a specific host?  (maybe it should be different when running
---	on a different server... probably a fringe case)
---
-CREATE TABLE `cli_logger`.`cli_script_table` (
-  `script_id` int  NOT NULL AUTO_INCREMENT,
-  `script_name` text  NOT NULL,
-  `max_run_time` int  NOT NULL,
-  `average_run_time` int  NOT NULL,
-  PRIMARY KEY (`script_id`)
-)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+-- 
+-- Table structure for table `cli_internal_log_table`
+-- 
 
+CREATE TABLE `cli_internal_log_table` (
+  `internal_log_id` int(11) NOT NULL auto_increment,
+  `log_timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `log_data` text NOT NULL,
+  PRIMARY KEY  (`internal_log_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
---
--- Log Table
---
-CREATE TABLE `cli_logger`.`cli_log_table` (
-  `log_id` int  NOT NULL AUTO_INCREMENT,
-  `script_id` int  NOT NULL,
-  `full_command` text  NOT NULL,
-  `host_id` int  NOT NULL,
-  `start_time` timestamp  NOT NULL DEFAULT NOW(),
-  `end_time` timestamp ,
-  `output` text ,
-  `errors` text ,
-  `exit_code` integer ,
-  PRIMARY KEY (`log_id`),
-  CONSTRAINT `cli_log_table_script_id_fkey` FOREIGN KEY `cli_log_table_script_id_fkey` (`script_id`)
-    REFERENCES `cli_script_table` (`script_id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
-  CONSTRAINT `cli_log_table_host_id_fkey` FOREIGN KEY `cli_log_table_host_id_fkey` (`host_id`)
-    REFERENCES `cli_host_table` (`host_id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT
-)
-ENGINE = InnoDB;
+-- 
+-- Table structure for table `cli_log_table`
+-- 
 
+CREATE TABLE `cli_log_table` (
+  `log_id` int(11) NOT NULL auto_increment,
+  `script_id` int(11) NOT NULL,
+  `full_command` text NOT NULL,
+  `host_id` int(11) NOT NULL,
+  `start_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  `end_time` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `output` text,
+  `errors` text,
+  `exit_code` int(11) default NULL,
+  PRIMARY KEY  (`log_id`),
+  KEY `cli_log_table_script_id_fkey` (`script_id`),
+  KEY `cli_log_table_host_id_fkey` (`host_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `cli_script_table`
+-- 
+
+CREATE TABLE `cli_script_table` (
+  `script_id` int(11) NOT NULL auto_increment,
+  `script_name` text NOT NULL,
+  `max_run_time` int(11) NOT NULL,
+  `average_run_time` int(11) NOT NULL,
+  PRIMARY KEY  (`script_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- 
+-- Constraints for dumped tables
+-- 
+
+-- 
+-- Constraints for table `cli_log_table`
+-- 
+ALTER TABLE `cli_log_table`
+  ADD CONSTRAINT `cli_log_table_script_id_fkey` FOREIGN KEY (`script_id`) REFERENCES `cli_script_table` (`script_id`),
+  ADD CONSTRAINT `cli_log_table_host_id_fkey` FOREIGN KEY (`host_id`) REFERENCES `cli_host_table` (`host_id`);
+
+SET FOREIGN_KEY_CHECKS=1;
+
+COMMIT;
